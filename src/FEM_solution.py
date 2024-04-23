3f359a35818161bec77d144326900e95d1a4ea42
Manuel Pena <mpena991@gmail.com>
2024-04-23 18:02:44 +0200

    penetrable compared with FEM

--- src/FEM_solution.py
+++ src/FEM_solution.py
@@ -3,7 +3,7 @@ def u_FEM_SOUNDSOFT(R = 10., H=1., rad = 0.2, c = (0.,0.), n=0, k=8., X=None, Y=
 
     from netgen.geom2d import SplineGeometry
     from ngsolve import Mesh, H1, pml, BilinearForm, SymbolicBFI, grad, CoefficientFunction
-    from ngsolve import exp, cos, sqrt, GridFunction, BND, LinearForm, x, y 
+    from ngsolve import exp, cos, sqrt, GridFunction, BND, LinearForm, x, y
     import numpy as np
 
 
@@ -88,6 +88,103 @@ def u_FEM_SOUNDSOFT(R = 10., H=1., rad = 0.2, c = (0.,0.), n=0, k=8., X=None, Y=
                     pass
                 else:
                     U_tot[i,j] = u_tot(mesh(x_,y_))
+        return U_tot
+    else:
+        return u_tot, mesh
+
+def u_FEM_PENETRABLE(R = 10., H=1., rad = 0.2, c = (0.,0.), n=0, k_e=8., k_i = 12.,  X=None, Y=None):
+
+    from netgen.geom2d import SplineGeometry
+    from ngsolve import Mesh, H1, pml, BilinearForm, SymbolicBFI, grad, CoefficientFunction
+    from ngsolve import exp, cos, sqrt, GridFunction, BND, LinearForm, x, y, SymbolicLFI
+    import numpy as np
+
+
+    porder=5  # polynomial FEM order
+    delta_PML = 2 * 2*np.pi/k_e
+    geo = SplineGeometry()
+    hmax_e = 2*np.pi/k_e / 10
+    hmax_i = 2*np.pi/k_i / 10
+    p1,p2,p3,p4 = [ geo.AppendPoint(x,y) for x,y in [ (-R,-H), (R,-H),(R,H),(-R,H)]]
+    geo.Append (["line", p1, p2],leftdomain=1,rightdomain=0,bc="pipe")
+    geo.Append (["line", p2, p3],leftdomain=1,rightdomain=2)
+    geo.Append (["line", p3, p4],leftdomain=1,rightdomain=0,bc="pipe")
+    geo.Append (["line", p4, p1],leftdomain=1,rightdomain=3)
+    geo.AddCircle(c, rad, leftdomain=4, rightdomain=1)
+    p5,p6 = [ geo.AppendPoint(x,y) for x,y in [ (-R-delta_PML,-H),
+                                                (-R-delta_PML,H)]]
+    
+    geo.Append (["line", p5, p1],leftdomain=3,rightdomain=0,bc="pipe")
+    geo.Append (["line", p6, p5],leftdomain=3,rightdomain=0,bc="PML")
+    geo.Append (["line", p4, p6],leftdomain=3,rightdomain=0,bc="pipe")
+    p7,p8 = [ geo.AppendPoint(x,y) for x,y in [ (R+delta_PML,-H),
+                                                (R+delta_PML,H)]]
+    geo.Append (["line", p2, p7],leftdomain=2,rightdomain=0,bc="pipe")
+    geo.Append (["line", p7, p8],leftdomain=2,rightdomain=0,bc="PML")
+    geo.Append (["line", p8, p3],leftdomain=2,rightdomain=0,bc="pipe")
+    geo.SetMaterial(1, "fluid")
+    geo.SetMaterial(2, "PML_right")
+    geo.SetMaterial(3, "PML_left")
+    geo.SetMaterial(4, "scatterer")
+    geo.SetDomainMaxH(4,hmax_i) 
+    geo.SetDomainMaxH(3,hmax_e)
+    geo.SetDomainMaxH(2,hmax_e)
+    geo.SetDomainMaxH(1,hmax_e)
+    ngmesh=geo.GenerateMesh()
+    mesh = Mesh(ngmesh)
+
+    mesh.Curve(porder)
+
+    fes = H1(mesh, order=porder, complex=True, dirichlet="PML")
+
+    factor = 0.1
+
+    mesh.SetPML(pml.HalfSpace(point=(R,H),normal=(1,0),alpha=factor*(4+2*1j)),
+                    "PML_right")
+    mesh.SetPML(pml.HalfSpace(point=(-R,H),normal=(-1,0),alpha=factor*(4+2*1j)),
+                    "PML_left")
+
+    u  = fes.TrialFunction()
+    v  = fes.TestFunction()
+
+
+    k=CoefficientFunction([k_i if mat=="scatterer" else k_e for mat in mesh.GetMaterials()])
+    #scatter=CoefficientFunction([5 if mat=="scatter" else 1 if mat=="fluid" else 2 if mat=="PML_right" else 3  if mat=="PML_left" else 4 for mat in mesh.GetMaterials()])
+
+
+    a=BilinearForm(fes)
+    a += SymbolicBFI(grad(u)*grad(v) - k**2*u*v)
+
+    a.Assemble()
+    Ainv=a.mat.Inverse(freedofs=fes.FreeDofs())
+
+
+
+    u_inc = CoefficientFunction(0.+0.*1j)
+    beta_n = np.sqrt(complex(k_e**2 - (n*np.pi/H)**2))
+    u_inc += exp( 1j*beta_n*x)*cos(n*np.pi*y)
+    u_sc = GridFunction(fes)
+
+    f = LinearForm(fes)
+    f += SymbolicLFI(k_e**2*( (k_i/k_e)**2 - 1)*u_inc*v, definedon=mesh.Materials('scatterer'))
+    f.Assemble()
+    
+
+    u_sc.vec.data += Ainv * f.vec
+    u_tot = u_sc + u_inc
+
+    if X is not None:
+        Ny, Nx = X.shape
+        x = X[0,:]
+        y = Y[:,0]
+        
+        U_tot = np.zeros_like(X, dtype=np.complex128)
+
+        for i in range(Ny):
+            for j in range(Nx):
+                x_ = x[j]
+                y_ = y[i]
+                U_tot[i,j] = u_tot(mesh(x_,y_))
         return U_tot
     else:
         return u_tot, mesh
