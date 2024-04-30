2fc350828ac26185c118f648437704a34ef9f7a9
Manuel Pena <mpena991@gmail.com>
2024-04-30 02:00:47 +0200

    fundamental solution working both with modes and images

--- src/exact_solutions.py
+++ src/exact_solutions.py
@@ -6,15 +6,18 @@ def H01(x):
     return j0(x) + 1j*y0(x)
 
 def GreenFunctionImages(k, H, XY, x_0, y_0, M = 100):
-    n = np.arange(-M,M+1)
-    xy_0 = np.stack([np.full(2*M+1,fill_value=x_0), 2*n*H + (1 - 2* (n%2) )*y_0],axis=1)
+    n = np.arange(0,M)
+    minus_1_to_n = 1 - 2* (n%2) 
+    xy_0p = np.stack([np.full(M,fill_value=x_0), 2*H*np.ceil(n/2) + minus_1_to_n*y_0],axis=1)
+    xy_0m = np.stack([np.full(M,fill_value=x_0), -2*H*np.ceil(n/2) - minus_1_to_n*y_0],axis=1)
+    xy_0 = np.vstack([xy_0p, xy_0m])
     G = 1j/4*np.sum(H01(k*cdist(XY, xy_0)),axis=-1)
     return G
 
 def GreenFunctionModes(k, H, XY, x_0, y_0, M = 20):
     n = np.arange(0,M+1)
     beta_n = np.emath.sqrt(k**2 - (n*np.pi/H)**2)
-    norm = np.full_like(n,1/H)
-    norm[0] = 1/2*H
+    norm = np.full_like(n,2/H)
+    norm[0] = 1/H
     G = -np.sum( norm*np.exp(1j*np.outer(np.abs(XY[:,0] - x_0),beta_n))/(2*1j*beta_n)*np.cos(np.pi*np.outer(XY[:,1],n)/H)*np.cos(n*np.pi*y_0/H), -1)
     return G
