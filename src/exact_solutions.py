a628c04998dd3adc43024770ee595d8d0a1cd4a6
Manuel Pena <mpena991@gmail.com>
2024-05-03 14:20:33 +0200

    comparison in y axes does nothing

--- src/exact_solutions.py
+++ src/exact_solutions.py
@@ -1,7 +1,8 @@
 from scipy.spatial.distance import cdist
 from scipy.special import j0, y0
 import numpy as np
-
+from numpy import cos, pi, exp, abs
+from numpy.lib.scimath import sqrt
 def H01(x):
     return j0(x) + 1j*y0(x)
 
@@ -16,8 +17,8 @@ def GreenFunctionImages(k, H, XY, x_0, y_0, M = 100):
 
 def GreenFunctionModes(k, H, XY, x_0, y_0, M = 20):
     n = np.arange(0,M+1)
-    beta_n = np.emath.sqrt(k**2 - (n*np.pi/H)**2)
+    beta_n = sqrt(k**2 - (n*np.pi/H)**2)
     norm = np.full_like(n,2/H)
-    norm[0] = 1/H
-    G = -np.sum( norm*np.exp(1j*np.outer(np.abs(XY[:,0] - x_0),beta_n))/(2*1j*beta_n)*np.cos(np.pi*np.outer(XY[:,1],n)/H)*np.cos(n*np.pi*y_0/H), -1)
+    norm[0] = 1/H 
+    G = -np.sum( norm*exp(1j*np.outer( abs(XY[:,0] - x_0),beta_n)) / (2*1j*beta_n) * cos( pi*np.outer(XY[:,1],n)/H) * cos(n* pi*y_0/H), -1)
     return G
