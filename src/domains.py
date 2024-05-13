b266ad44c9dfe2e13a77c8481451a352ac2ce873
Manuel Pena <mpena991@gmail.com>
2024-05-13 02:20:05 +0200

    rectangles fixed, considering changing to pcolormesh representation

--- src/domains.py
+++ src/domains.py
@@ -44,7 +44,7 @@ class Waveguide:
                     self.geo.AddRectangle(p1=(c[0]-length/2,c[1]-height/2), p2=(c[0]+length/2,c[1]+height/2), leftdomain=2, rightdomain=1)
                     self.geo.SetMaterial (2, "Omega_i")
                 else:
-                    self.geo.AddRectangle(p1=(c[0]-length/2,c[1]-height/2), p2=(c[0]+length/2,c[1]+height/2), leftdomain=0, rightdomain=1)
+                    self.geo.AddRectangle(p1=(c[0]-length/2,c[1]-height/2), p2=(c[0]+length/2,c[1]+height/2), bc="D_Omega", leftdomain=0, rightdomain=1)
                     
     def generate_mesh(self, h_max = 1.):
         Omega = Mesh(self.geo.GenerateMesh(maxh= h_max))
