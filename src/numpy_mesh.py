64ae9cc4f5b518be97a3f96e2bfbe44642e85f21
Manuel Pena <mpena991@gmail.com>
2024-05-20 01:38:31 +0200

    it may work

---
+++ src/numpy_mesh.py
@@ -0,0 +1,21 @@
+'''module to implement what is going to be the basic "numpy mesh"'''
+import numpy as np
+from labels import EdgeType
+# from enum import Enum, auto
+
+# # not sure yet where to put this
+# class EdgeType(Enum):
+#     INNER = auto()
+#     GAMMA = auto()
+#     SIGMA_L = auto()
+#     SIGMA_R = auto()
+#     D_OMEGA = auto()
+#     COVER = auto()
+
+
+edge_dtype = np.dtype( [("midpoint", np.float64,2),
+                        ("normal", np.float64, 2),
+                        ("length", np.float64, 1),
+                        ("label", EdgeType, 1)])
+
+
