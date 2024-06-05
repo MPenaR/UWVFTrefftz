67989b0f5273127dd80bd13f760b1bd0c8d8bf33
Manuel Pena <mpena991@gmail.com>
2024-06-05 15:47:02 +0200

    firs code for absorbing media

--- src/Trefft_tools.py
+++ src/Trefft_tools.py
@@ -290,11 +290,11 @@ def Sigma_nonlocal(phi, psi, edge_u, edge_v, k, H, d_2, Np=15):
     return  I1 + I2 + I3
 
 
-# def absorption_term( phi, psi, r_A, r_B, r_C, k, a, b):
-#     n_I = np.imag()   
-#     I = 2*1j*k**2*fek3_int(lambda x, y : np.exp(1j*k*n), r_A=r_A, r_B=r_B, r_C=r_C)
+def absorption_term( phi, psi, r_A, r_B, r_C, k, a, b):
+    n_I = np.imag()   
+    I = 2*1j*k**2*fek3_int(lambda x, y : np.exp(1j*k*n), r_A=r_A, r_B=r_B, r_C=r_C)
 
-#     return I
+    return I
     
 
 
