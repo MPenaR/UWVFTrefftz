4676113876571c19193f63b1b46ab92e47e5505b
Manuel Pena <mpena991@gmail.com>
2024-08-24 07:11:02 +0200

    thiner lines and smaller markers

--- src/paper_plots.py
+++ src/paper_plots.py
@@ -30,7 +30,10 @@ from matplotlib import ticker as mticker
 import matplotlib
 
 matplotlib.style.use({
-    "font.size" : 7
+    "font.size" : 7,
+    "lines.linewidth" : 1.0,
+    "lines.markersize":      3 
+
 })
 
 def plot_hp_convergence(errors, hs, N_ths, kappa_e, N_modes, H, title = None, filename=None, ax = None):
