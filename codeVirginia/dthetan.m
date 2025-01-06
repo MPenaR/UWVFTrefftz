8ec3da7234949719798467759efae9c55a2830cb
Manuel Pena <mpena991@gmail.com>
2025-01-06 02:17:02 +0100

    removed code from Virginia and TDwaveguide 2 check

--- codeVirginia/dthetan.m
+++
@@ -1,7 +0,0 @@
-function y=dthetan(x2,n,H)
-    if n==0
-        y=0.*x2;
-    else
-        y=-(sqrt(2/H)*n*pi/H)*sin(n*pi*x2/H);
-    end
-end
--- codeVirginia/thetan.m
+++
@@ -1,7 +0,0 @@
-function y=thetan(x2,n,H)
-    if n==0
-        y=sqrt(1/H)+0.*x2;
-    else
-        y=sqrt(2/H)*cos(n*pi*x2/H);
-    end
-end
