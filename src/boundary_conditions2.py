cd2ad08239d3b4338ff28fd813e8f7027c151382
Manuel Pena <mpena991@gmail.com>
2025-05-20 01:22:20 +0200

    defining boundary conditions as a class

---
+++ src/boundary_conditions2.py
@@ -0,0 +1,13 @@
+"""
+Module for defining boundary conditions.
+"""
+
+class BoundaryCondition:
+    pass 
+
+
+class DirichletCondition(BoundaryCondition):
+    pass
+
+class NeumannCondition(BoundaryCondition):
+    pass
