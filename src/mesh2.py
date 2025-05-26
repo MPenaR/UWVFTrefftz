75fa388342556e127939b3d978327efdeb31b3d5
Manuel Pena <mpena991@gmail.com>
2025-05-27 01:36:37 +0200

    from triangulation and to triangulation seems to work, still from netgen missing

--- src/mesh2.py
+++ src/mesh2.py
@@ -3,6 +3,7 @@
 import numpy as np
 import numpy.typing as npt
 from matplotlib.tri import Triangulation
+from itertools import combinations
 
 float_array = npt.NDArray[np.float64]
 complex_array = npt.NDArray[np.complex128]
@@ -24,24 +25,25 @@ class SurfaceMesh:
         self.edges = edges
         self.faces = faces
 
-    @staticmethod
+    @classmethod
     def from_numpy(cls, points: float_array,
                    edges: int_array,
                    faces: int_array):
         """it assumes the points/edges/faces IDs are their row index.
         edges is a  n_edges x 2 array, refering to the index of its ends
-        faces is a n_faces x 3 array, refereing to the index of its edges"""
+        faces is a n_faces x 3 array, refering to the index of its edges"""
 
         vertices = np.arange(len(points), dtype=np.int64)
         edges = edges
         faces = faces
         return cls(points, vertices, edges, faces)
 
-    @staticmethod
+    @classmethod
     def from_netgen(cls, points, edges, faces):
         pass
 
-    @staticmethod
+  
+    @classmethod
     def from_Triangulation(cls, tri: Triangulation):
         """
         Expects a Matplotlib triangulation
@@ -50,30 +52,59 @@ class SurfaceMesh:
         points = np.column_stack([tri.x, tri.y])
         vertices =  np.arange(len(points), dtype=np.int64)
         edges = tri.edges
-        triangles
+        
+        edges_dict = generate_edges_dict(edges)
+
+        triangles = tri.triangles
+
+        faces = np.zeros([len(triangles), 3], dtype=np.int64)
+        for n, T in enumerate(triangles):
+            faces[n] = [edges_dict[frozenset(c)] for c in combinations(T, 2)]
+
 
 
         return cls(points, vertices, edges, faces )
 
-    def to_matplotlib(self):
-        x = self.points[self.vertices["ID"], 0]
-        y = self.points[self.vertices["ID"], 1]
-        Triangles = self.faces["vertices"]
-        return Triangulation(x, y, Triangles)
+    def to_matplotlib(self) -> Triangulation:
+        x = self.points[:, 0]
+        y = self.points[:, 1]
+        triangles = np.zeros((len(self.faces), 3), dtype=np.int64)
+        edges = self.edges
+        for n, f in enumerate(self.faces):
+            triangles[n] = list(set(edges[f[0]]) | set(edges[f[1]]) | set(edges[f[2]]))
+        return Triangulation(x, y, triangles)
+
+    @property
+    def edges_dict(self) -> dict[frozenset,int]:
+        return generate_edges_dict(self.edges)
+
+#def generate_test_mesh() -> SurfaceMesh:
 
 
+def generate_edges_dict(edges: int_array) -> dict[frozenset, int]:
+    return {frozenset(e): i for i, e in enumerate(edges)}
 
-def generate_test_mesh() -> SurfaceMesh:
+if __name__ == "__main__":
+
     points = np.array([[0, 0],
-                   [1, 0],
-                   [1, 1],
-                   [0, 1]], dtype=np.float64)
+                       [1, 0],
+                       [1, 1],
+                       [0, 1]], dtype=np.float64)
 
     edges = np.array([[0, 1],
-                    [1, 2],
-                    [2, 3],
-                    [3, 0],
-                    [0, 2]], dtype=np.int32) 
+                      [1, 2],
+                      [2, 3],
+                      [3, 0],
+                      [0, 2]], dtype=np.int32) 
 
-    faces = np.array([[0, 1, 2],
-                    [0, 2, 3]], dtype=np.int32)
+    triangles = np.array([[0, 1, 2],
+                          [0, 2, 3]], dtype=np.int32)
+    
+    tri = Triangulation(x=points[:,0], y=points[:,1], triangles=triangles)
+    S = SurfaceMesh.from_Triangulation(tri)
+    print(S.edges)
+    print(S.faces)
+
+    tri_2 = S.to_matplotlib()
+    print(f'{tri_2.triangles=}')
+    print(f'{tri_2.x=}, {tri_2.y=}')
